5134ca48779ee632272357000b242af16dde4a52
ashraful-fahim <ashraful30june@gmail.com>
2025-05-31 22:25:07 +0600

    explored the ways of styling in react

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,38 @@ function App() {
       <h1>Vite + React</h1>
       <Person></Person>
       <Quality></Quality>
+      <Student></Student>
+      <Student></Student>
+      <Person></Person>
+      <Developer name="Mozumder" tech="JS"></Developer>
+      <Developer name="Rajon" tech="python"></Developer>
+      <Developer name="Shabana" tech="Java"></Developer>
+
     </>
+  )
+}
+
+function Developer(props) {
+  return (
+    <div style={{
+      border: "2px solid green",
+      borderRadius: "20px"
+    }}>
+      <h3>Developer: {props.name}</h3>
+      <p>Technology: {props.tech}</p>
+    </div>
   )
 }
 
 function Person() {
   const name = "Fahim";
   const age = 27;
+  const personStyle = {
+    color: "lightGreen",
+    textAlign: "center",
+  }
   return (
-    <p>I am {name} and I am {age} years old.</p>
+    <p style={personStyle}>I am {name} and I am {age} years old.</p>
   )
 }
 
@@ -28,6 +51,15 @@ function Quality() {
       <p>I know {lang1}. I have also learned {lang2}.</p>
     </div>
   ) 
+}
+
+function Student() {
+  return (
+    <div className='student '>
+      <p>Name: </p>
+      <p>Dept.: </p>
+    </div>
+  )  
 }
 
 export default App
